4592407ae79d3610cbc0c06cbb233f78ac206c24
steve.deng <steved@mydomain.com>
2022-09-15 09:15:33 +1000

    save changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { actionCreators } from './state';
-import { RootState } from './state/reducers';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state";
+import { RootState } from "./state/reducers";
+import Button from "@mui/material/Button";
 
 function App() {
-
-  const state = useSelector((state: RootState) => state.bank)
+  const state = useSelector((state: RootState) => state.bank);
   const dispatch = useDispatch();
 
-  const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(actionCreators, dispatch)
+  const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
 
   return (
     <div className="App">
       <h1>{state}</h1>
-      <button onClick={() => depositMoney(1000)}>Deposit</button>
-      <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
-      <button onClick={() => bankrupt()}>Bankrupt</button>
+      <Button
+        onClick={() => depositMoney(1000)}
+        variant="contained"
+        color="success"
+      >
+        Deposit
+      </Button>
+      <Button onClick={() => withdrawMoney(1000)} variant="contained">
+        Withdraw
+      </Button>
+      <Button onClick={() => bankrupt()} variant="outlined" color="error">
+        Bankrupt
+      </Button>
     </div>
   );
 }
