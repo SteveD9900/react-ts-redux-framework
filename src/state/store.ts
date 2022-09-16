3c44e83d2b1a4a82ca38af1c6d5fab1fcf456533
steve.deng <steved@mydomain.com>
2022-09-16 14:53:25 +1000

    convert createStore into configureStore with redux toolkit

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,8 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk"
 import reducers from "./reducers";
 
-export const store = createStore(
-    reducers,
-    {},
-    applyMiddleware(thunk)
-)
+export const store = configureStore({
+    reducer: reducers
+})
